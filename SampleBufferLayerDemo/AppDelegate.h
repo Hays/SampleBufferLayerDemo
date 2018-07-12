59cff7ca3a3451375347512968379af31b649657
Hays <huangwenxi@cvte.com>
2018-07-12 14:17:00 +0800

    [Modify]换oc实现

---
+++ SampleBufferLayerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleBufferLayerDemo
+//
+//  Created by 黄文希 on 2018/7/12.
+//  Copyright © 2018 Hays. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
